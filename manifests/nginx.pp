d56aba5f4c3d0a1d89b25c78983321df102b5c36
Michael Maclean <michael@no-surprises.co.uk>
2013-03-14 23:23:28 +0000

    Add PHP modules, and MySQL server

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -2,6 +2,13 @@ group { "puppet":
 	ensure => "present",
 }
 
+$phpModules = ['php5-cli', 'php5-curl', 'php5-dev', 'php5-gd', 'php5-imagick', 'php5-mcrypt', 'php5-memcached', 'php5-mysqlnd', 'php5-sqlite', 'php5-tidy']
+
+package { $phpModules:
+  ensure => latest,
+  notify => Service['php5-fpm']
+}
+
 class { 'phpfpm': }
 
 nginx::vhost { 'dev.mgdm':
@@ -10,4 +17,8 @@ nginx::vhost { 'dev.mgdm':
 	template => 'nginx/vhost.php.conf.erb',
 }
 
+class { 'mysql': }
 
+class { 'mysql::server': 
+	config_hash => { 'root_password' => 'helloiamroot' }
+}
