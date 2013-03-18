3b25283682caba4c94e0560e62b6d0b02d1f58f0
Michael Maclean <michael.maclean@stv.tv>
2013-03-18 09:59:23 +0000

    Make sure nginx gets started properly

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -4,39 +4,34 @@ group { "puppet":
 
 class { 'apt': }
 
-apt::ppa { 'ppa:ondrej/php5': }
+include phpPackages
+class phpPackages {
+	apt::ppa { 'ppa:ondrej/php5': }
 
-$phpModules = [
-	'php-apc',
-	'php5-cli',
-	'php5-curl',
-	'php5-dev',
-	'php5-gd',
-	'php5-imagick',
-	'php5-mcrypt',
-	'php5-memcached',
-	'php5-mysqlnd',
-	'php5-sqlite',
-	'php5-tidy',
-	'php5-xdebug',
-]
+	$phpModules = [
+		'php-apc',
+		'php5-cli',
+		'php5-curl',
+		'php5-dev',
+		'php5-gd',
+		'php5-imagick',
+		'php5-mcrypt',
+		'php5-memcached',
+		'php5-mysqlnd',
+		'php5-sqlite',
+		'php5-tidy',
+		'php5-xdebug',
+		]
 
-package { $phpModules:
-  ensure => latest,
-  notify => Service['php5-fpm'],
-  require => Apt::Ppa['ppa:ondrej/php5'],
-}
-
-service {'apache2':
-  ensure => stopped
-}
-
-package {'apache2':
-  ensure => absent,
+		package { $phpModules:
+			ensure => latest,
+			notify => Service['php5-fpm'],
+			require => Apt::Ppa['ppa:ondrej/php5'],
+		}
 }
 
 class { 'phpfpm':
-  require => Apt::Ppa['ppa:ondrej/php5'],
+  require => Class['phpPackages']
 }
 
 nginx::vhost { 'dev.mgdm':
@@ -44,6 +39,17 @@ nginx::vhost { 'dev.mgdm':
 	index => 'index.php',
 	template => 'nginx/vhost.php.conf.erb',
 	fastcgi_socket => 'unix:/var/run/php5-fpm.sock',
+}
+
+service { 'apache2':
+	ensure => stopped,
+    require => Class['phpPackages'],
+	notify => Service['nginx']
+}
+
+package { 'apache2':
+	ensure => absent,
+	require => Service['apache2']
 }
 
 class { 'mysql': }
